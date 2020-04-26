35ad7901130d91183ce87489aa62273664ac6149
LeiYangGH <675686066@qq.com>
2020-04-26 09:04:21 +0800

    done

--- random_st_connect.py
+++ random_st_connect.py
@@ -1,11 +1,7 @@
 import os
-import re
+import random
 from collections import defaultdict
 
-reg_graph_content = re.compile(r'^(?:\d+\n){3}\n(?:(?:\d\s*)+\n)')
-
-
-# reg_graph_content = re.compile(r'^(?:\d+\n){3}\n')
 
 def read_graph(filename):
     with open(filename, 'r') as f:
@@ -31,9 +27,40 @@ def read_graph(filename):
                 else:
                     assert is_connected == '0'
         # print(graph)
-        return graph
+        return (graph, vertices_count, s_vertex, t_vertex)
+
+
+def randomised_st_connectivity(G, n, s, t):
+    step_count = 0
+    current_vertex = s
+    while (current_vertex != t) and (step_count < 2 * pow(n, 3)):
+        current_vertex = random.choice(G[current_vertex])
+        step_count = step_count + 1
+    if current_vertex == t:
+        return (True, step_count)
+    else:
+        return (False, step_count)
+
+
+def read_find_report(graph_file, report_file):
+    (graph, vertices_count, s_vertex, t_vertex) = read_graph(graph_file)
+    found, step_count = randomised_st_connectivity(graph, vertices_count, s_vertex, t_vertex)
+    with open(report_file, "w") as fw:
+        fw.write('input graph file: {} \n'.format(graph_file))
+        fw.write('vertices_count: {} \n'.format(vertices_count))
+        fw.write('s: {} \n'.format(s_vertex))
+        fw.write('t: {} \n'.format(t_vertex))
+        fw.write('Found: {}\n'.format(found))
+        fw.write('step_count: {}\n'.format(step_count))
+    print('Done processing {} and report in {}.'.format(graph_file, report_file))
 
 
 if __name__ == "__main__":
-    graph = read_graph(r'testgraphs/testgraph1.txt')
-    print(graph)
+    graphs_dir = 'testgraphs'
+    reports_dir = 'reports'
+    if not os.path.isdir(reports_dir):
+        os.mkdir(reports_dir)
+    for filename in os.listdir(graphs_dir):
+        graph_fullname = os.path.join(graphs_dir, filename)
+        report_fullname = os.path.join(reports_dir, filename)
+        read_find_report(graph_fullname, report_fullname)
